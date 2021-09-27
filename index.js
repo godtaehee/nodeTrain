0accac0c10a51d5843f862ea59392dea2d6bfb5c
taehee <wonderfulhuman@naver.com>
2021-09-27 18:21:49 +0900

    Execute Server with http module

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// 에러를 해결하려 들지말고 그냥 process.exit로 프로세스를 끝내버리자.
-process.on('uncaughtException', (err) => {
-  console.error('예기치 못한 에러', err);
-});
+const http = require('http');
 
-setInterval(() => {
-  throw new Error('서버를 고장내주마!');
-}, 1000);
-
-setTimeout(() => {
-  console.log('실행됩니다.');
-}, 2000);
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      'Content-type': 'text/html; charset=utf-8',
+    });
+    res.write('<h1>Hello Node!</h1>');
+    res.end('<p>Hello Server!</p>');
+  })
+  .listen(8080, () => {
+    console.log('http://localhost:8080');
+  });
