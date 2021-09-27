fe9ee03ec7bddb585d6979dfefd97023979f73ba
taehee <wonderfulhuman@naver.com>
2021-09-27 17:14:17 +0900

    Thread Pool

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const fs = require('fs');
+const crypto = require('crypto');
 
-const readStream = fs.createReadStream('writeme2.txt');
-const writeStream = fs.createWriteStream('writeme3.txt');
-readStream.pipe(writeStream);
+const pass = 'pass';
+const salt = 'salt';
+
+const start = Date.now();
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('1: ', Date.now() - start);
+});
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('2: ', Date.now() - start);
+});
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('3: ', Date.now() - start);
+});
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('4: ', Date.now() - start);
+});
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('5: ', Date.now() - start);
+});
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('6: ', Date.now() - start);
+});
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('7: ', Date.now() - start);
+});
+
+crypto.pbkdf2(pass, salt, 1000000, 128, 'sha512', () => {
+  console.log('8: ', Date.now() - start);
+});
