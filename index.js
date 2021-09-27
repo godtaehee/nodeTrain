6f31bfae531f671b96cec00c80014506603a06b1
taehee <wonderfulhuman@naver.com>
2021-09-27 16:50:37 +0900

    Write file with pipe

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const fs = require('fs');
 
-const writeStream = fs.createWriteStream('./writeme2.txt');
-writeStream.on('finish', () => {
-  console.log('파일 쓰기 완료');
-});
-
-writeStream.write('이 글을 씁니다 \n');
-writeStream.write('한 번 더 씁니다.');
-writeStream.end();
+const readStream = fs.createReadStream('writeme2.txt');
+const writeStream = fs.createWriteStream('writeme3.txt');
+readStream.pipe(writeStream);
