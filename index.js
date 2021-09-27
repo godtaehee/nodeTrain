e85d70face75c17d008258bcfc07f37b10754812
taehee <wonderfulhuman@naver.com>
2021-09-27 22:04:22 +0900

    Clusting

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
+const cluster = require('cluster');
 const http = require('http');
-const fs = require('fs').promises;
-const url = require('url');
-const qs = require('querystring');
+const numCPUs = require('os').cpus().length;
 
-const parseCookies = (cookie = '') =>
-  cookie
-    .split(';')
-    .map((v) => v.split('='))
-    .reduce((acc, [k, v]) => {
-      acc[k.trim()] = decodeURIComponent(v);
-      return acc;
-    }, {});
+console.log(numCPUs);
+if (cluster.isMaster) {
+  console.log(`마스터 프로세스 아이디: ${process.pid}`);
 
-http
-  .createServer(async (req, res) => {
-    const cookies = parseCookies(req.headers.cookie); // { mycookie: 'test' }
-    // 주소가 /login으로 시작하는 경우
-    if (req.url.startsWith('/login')) {
-      const { query } = url.parse(req.url);
-      const { name } = qs.parse(query);
-      const expires = new Date();
-      // 쿠키 유효 시간을 현재시간 + 5분으로 설정*
-      expires.setMinutes(expires.getMinutes() + 5);
-      res.writeHead(302, {
-        'Location': '/',
-        'Set-Cookie': `name=${encodeURIComponent(
-          name
-        )}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
-      });
-      res.end();
-      // name이라는 쿠키가 있는 경우
-    } else if (cookies.name) {
-      res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-      res.end(`${cookies.name}님 안녕하세요`);
-    } else {
-      try {
-        const data = await fs.readFile('./cookie2.html');
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(data);
-      } catch (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end(err.message);
-      }
-    }
-  })
-  .listen(8084, () => {
-    console.log('http://localhost:8084');
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`${worker.process.pid} 번 워커가 종료되었습니다.`);
+    console.log('code', code, 'signal', signal);
+    cluster.fork();
   });
+} else {
+  http
+    .createServer((req, res) => {
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+      });
+      res.write('<h1>Hello Node!</h1>');
+      res.end('<p>Hello Cluster!</p>');
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    })
+    .listen(8080);
+  console.log(`${process.pid}번 워커 실행`);
+}
