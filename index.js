600643a90241374381b5b467f50a84b26ff17987
taehee <wonderfulhuman@naver.com>
2021-09-27 16:14:06 +0900

    fs.readFileSync는 순서대로 실행은시켜주지만 읽는 파일의 용량이 많아지면 그거 다 읽느라 나머지를 처리 못한다

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 const fs = require('fs');
 
 console.log('시작');
-fs.readFile('./readme.txt', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log('1번');
-  console.log(data.toString());
-});
-
-fs.readFile('./readme.txt', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log('2번');
-  console.log(data.toString());
-});
-
-fs.readFile('./readme.txt', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log('3번');
-  console.log(data.toString());
-});
-
+let data = fs.readFileSync('./readme.txt');
+console.log('1번', data.toString());
+data = fs.readFileSync('./readme.txt');
+console.log('2번', data.toString());
+data = fs.readFileSync('./readme.txt');
+console.log('3번', data.toString());
 console.log('끝');
